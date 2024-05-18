850ad5f13f525bd8f754c0f0deddd0d356157448
denvergeeks <jen@denvergeeks.com>
2024-05-17 18:27:22 -0600

    Update header-logout-button.js

--- javascripts/discourse/components/header-logout-button.js
+++ javascripts/discourse/components/header-logout-button.js
@@ -1,6 +1,11 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
+
+
+
+
+
+
 import { inject as service } from "@ember/service";
 
 export default class HeaderLogoutButton extends Component {
