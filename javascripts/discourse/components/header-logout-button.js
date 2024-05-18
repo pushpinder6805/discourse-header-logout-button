a2430b1d728e7d775529bdab7d10cb7b88339961
denvergeeks <jen@denvergeeks.com>
2024-05-17 18:17:21 -0600

    Update header-logout-button.js

--- javascripts/discourse/components/header-logout-button.js
+++ javascripts/discourse/components/header-logout-button.js
@@ -24,15 +24,7 @@ export default class HeaderLogoutButton extends Component {
     }
   }
 
-  handleDocumentClick(event) {
-    const dropdown = document.querySelector(".header-user-new__menu");
-    const isClickInside = dropdown.contains(event.target);
 
-    if (!isClickInside) {
-      this.isActive = false;
-      document.removeEventListener("click", this.handleDocumentClick);
-    }
-  }
 
 
 }
