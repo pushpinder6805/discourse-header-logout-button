61c6ceb4a1dd1eb1ddd990c685143a6b9c1e6994
denvergeeks <jen@denvergeeks.com>
2024-05-17 19:32:24 -0600

    Update header-logout-button.js

--- javascripts/discourse/widgets/header-logout-button.js
+++ javascripts/discourse/widgets/header-logout-button.js
@@ -3,7 +3,7 @@ import RenderGlimmer from "discourse/widgets/render-glimmer";
 import { createWidget } from "discourse/widgets/widget";
 
 export default createWidget("header-logout-button", {
-  tagName: "li.header-logout-button",
+  tagName: "li.btn.btn-icon-text.btn-primary.btn-small.login-button.header-logout-button",
 
   html() {
     return [new RenderGlimmer(this, "div", hbs`<HeaderLogoutButton />`)];
