ba07e23c1073f15c22e40ddef229f7480a630c1e
denvergeeks <jen@denvergeeks.com>
2024-05-17 19:14:08 -0600

    Delete javascripts/discourse/components/header-logout-button.js

--- javascripts/discourse/components/header-logout-button.js
+++
@@ -1,8 +0,0 @@
-import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-import { inject as service } from "@ember/service";
-
-export default class HeaderLogoutButton extends Component {
-  @service currentUser;
-  @tracked isActive = false;
-}
