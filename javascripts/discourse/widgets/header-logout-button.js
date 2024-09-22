b7f6980dfb66696495995cdd8d07006ff674ea16
denvergeeks <jen@denvergeeks.com>
2024-09-22 12:07:01 -0600

    Delete javascripts/discourse/widgets/header-logout-button.js

--- javascripts/discourse/widgets/header-logout-button.js
+++
@@ -1,9 +0,0 @@
-import { hbs } from "ember-cli-htmlbars";
-import RenderGlimmer from "discourse/widgets/render-glimmer";
-import { createWidget } from "discourse/widgets/widget";
-
-export default createWidget("header-logout-button", {
-  html() {
-    return [new RenderGlimmer(this, "div", hbs`<HeaderLogoutButton />`)];
-  },
-});
