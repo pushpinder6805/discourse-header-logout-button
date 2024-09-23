7a3a916201fb53392f25bb48e4d9832037dd2ae2
denvergeeks <jen@denvergeeks.com>
2024-09-22 19:48:17 -0600

    Add files via upload

---
+++ javascripts/discourse/initializers/header-edit.js
@@ -0,0 +1,15 @@
+import { h } from "virtual-dom";
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { iconNode } from "discourse-common/lib/icon-library";
+import I18n from "discourse-i18n";
+
+export default {
+  initialize() {
+    withPluginApi("1.28.0", (api) => {
+      const currentUser = api.container.lookup("service:current-user");
+        if (currentUser !== null) {
+         api.addToHeaderIcons("header-logout-button");
+        }
+    });
+  },
+};
