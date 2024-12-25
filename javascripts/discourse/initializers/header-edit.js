169c7ec8028c852af07a290b623f517037fc930c
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2024-12-25 13:44:00 +0530

    Update header-edit.js

--- javascripts/discourse/initializers/header-edit.js
+++ javascripts/discourse/initializers/header-edit.js
@@ -1,15 +1,40 @@
-import { h } from "virtual-dom";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { iconNode } from "discourse-common/lib/icon-library";
 import I18n from "discourse-i18n";
 
 export default {
+  name: "header-logout-button",
+
   initialize() {
     withPluginApi("1.28.0", (api) => {
       const currentUser = api.container.lookup("service:current-user");
-        if (currentUser !== null) {
-         api.addToHeaderIcons("header-logout-button");
-        }
+      if (currentUser) {
+        api.decorateWidget("header-icons:after", (helper) => {
+          return helper.h("li.header-logout-button", [
+            helper.h(
+              "button.btn.btn-icon.btn-flat.logout-button",
+              {
+                title: I18n.t("logout.title"),
+                onclick() {
+                  if (confirm(I18n.t("logout.confirm"))) {
+                    fetch("/session/logout", {
+                      method: "POST",
+                      headers: {
+                        "X-CSRF-Token": document
+                          .querySelector("meta[name='csrf-token']")
+                          .getAttribute("content"),
+                      },
+                    }).then(() => {
+                      window.location.reload();
+                    });
+                  }
+                },
+              },
+              iconNode("power-off")
+            ),
+          ]);
+        });
+      }
     });
   },
 };
